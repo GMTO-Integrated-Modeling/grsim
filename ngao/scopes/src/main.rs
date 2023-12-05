598f2827c9abeed983a91c41c101344de97510fd
Rod Conan <rconan@gmto.org>
2023-12-05 08:18:58 -0800

    NGAO SCOPES USE LATEST GRIDSCOPE BUILDER

--- ngao/scopes/src/main.rs
+++ ngao/scopes/src/main.rs
@@ -1,5 +1,5 @@
 use gmt_dos_clients_io::optics::SegmentPiston;
-use gmt_dos_clients_scope_client::{GridScope, Scope};
+use gmt_dos_clients_scope_client::GridScope;
 use interface::{units::NM, UID};
 
 #[derive(UID)]
@@ -11,30 +11,17 @@ pub enum SegmentAD7Piston {}
 #[derive(UID)]
 pub enum SegmentPistonInt {}
 
-const CLIENT_ADDRESS: &str = "0.0.0.0:0";
 const SERVER_IP: &str = "44.235.124.92";
 
 #[allow(unreachable_code)]
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     loop {
-        GridScope::new((2, 2))
-            .pin(
-                (0, 0),
-                Scope::new(SERVER_IP, CLIENT_ADDRESS).signal::<SegmentPiston<-9>>(5001)?,
-            )
-            .pin(
-                (0, 1),
-                Scope::new(SERVER_IP, CLIENT_ADDRESS).signal::<NM<SegmentPistonRecon>>(5002)?,
-            )
-            .pin(
-                (1, 0),
-                Scope::new(SERVER_IP, CLIENT_ADDRESS).signal::<SegmentAD7Piston>(5003)?,
-            )
-            .pin(
-                (1, 1),
-                Scope::new(SERVER_IP, CLIENT_ADDRESS).signal::<NM<SegmentPistonInt>>(5004)?,
-            )
+        GridScope::new((2, 2), SERVER_IP)
+            .pin::<SegmentPiston<-9>>((0, 0), 5001)?
+            .pin::<NM<SegmentPistonRecon>>((0, 1), 5002)?
+            .pin::<SegmentAD7Piston>((1, 0), 5003)?
+            .pin::<NM<SegmentPistonInt>>((1, 1), 5004)?
             .show();
     }
     Ok(())
