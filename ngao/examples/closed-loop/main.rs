66a0cb854132f48a3b8e52d136251284ecbf1b39
Rod Conan <rconan@gmto.org>
2024-03-14 02:46:43 +0000

    UPDATE CLOSED-LOOP EXAMPLE

--- ngao/examples/closed-loop/main.rs
+++ ngao/examples/closed-loop/main.rs
@@ -11,7 +11,7 @@ use gmt_dos_clients_crseo::{
     Calibration, DetectorFrame, GuideStar, OpticalModel, Processor, PyramidCalibrator,
     PyramidMeasurements, ResidualM2modes, WavefrontSensor, WavefrontStats,
 };
-use gmt_dos_clients_io::optics::{M2modes, SegmentWfeRms, WfeRms};
+use gmt_dos_clients_io::optics::{M2modes, SegmentWfeRms, Wavefront, WfeRms};
 use gmt_dos_clients_scope::server::{Monitor, Scope};
 use interface::Tick;
 
@@ -30,7 +30,7 @@ async fn main() -> anyhow::Result<()> {
 
     let n_lenslet = 92;
     // let (m2_modes, n_mode) = ("ASM_DDKLs_S7OC04184_675kls", 500);
-    let (m2_modes, n_mode) = ("M2_OrthoNormGS36p_KarhunenLoeveModes", 500);
+    let (m2_modes, n_mode) = ("M2_OrthoNormGS36p90_KarhunenLoeveModes", 500);
 
     // Pyramid definition
     let pym = Pyramid::builder()
@@ -47,40 +47,40 @@ async fn main() -> anyhow::Result<()> {
             .duration(5f64)
             .filepath(&data_repo.join("atmosphere.bin")),
     );
-    let optical_model = OpticalModel::builder()
+    let optical_model = OpticalModel::<Pyramid>::builder()
         .gmt(
             Gmt::builder()
                 .m1_truss_projection(false)
                 .m2(m2_modes, n_mode),
         )
         .source(pym.guide_stars(None))
+        .sensor(pym.clone())
         .atmosphere(atm_builder)
         .sampling_frequency(sampling_frequency as f64)
         .build()?;
     // Pyramid interaction matrix
     let file_name = format!("pymtor_{m2_modes}_{n_mode}.pkl");
     let path = data_repo.join(file_name);
-    let mut pymtor: PyramidCalibrator = if !path.exists() {
-        let pymtor = PyramidCalibrator::builder(pym.clone(), m2_modes, n_mode)
-            .n_gpu(8)
-            .build()?;
-        serde_pickle::to_writer(&mut File::create(&path)?, &pymtor, Default::default())?;
-        pymtor
-    } else {
-        println!("Loading {:?}", &path);
-        serde_pickle::from_reader(File::open(&path)?, Default::default())?
+    let calibrator: Calibration<PyramidCalibrator> = {
+        let filename = format! {"../data/pym-{m2_modes}-{n_mode}.bin"};
+        if let Ok(pymtor) = PyramidCalibrator::try_from(filename.as_str()) {
+            pymtor
+        } else {
+            let mut pymtor = PyramidCalibrator::builder(pym.clone(), m2_modes, n_mode)
+                .n_gpu(7)
+                .build()?;
+            pymtor.h00_estimator()?;
+            pymtor.save(filename)?
+        }
+        .into()
     };
-    assert_eq!(n_mode, pymtor.n_mode);
-    println!("{pymtor}");
+    assert_eq!(n_mode, calibrator.n_mode);
+    // println!("{pymtor}");
 
-    let pym = pym.build()?;
     // Pyramid data processor
-    let processor: Processor<_> = Processor::from(&pym);
-    // Pyramid wafefront sensor
-    let pyramid = WavefrontSensor::<_, 1>::new(pym);
+    let processor: Processor<_> = Processor::try_from(&pym)?;
     // Pyramid integral controller (modes>1)
-    let pym_ctrl = Integrator::<ResidualM2modes>::new((n_mode - 1) * 7).gain(0.5);
-    let stats = WavefrontStats::<1>::default();
+    let pym_ctrl = Integrator::<ResidualM2modes>::new(n_mode * 7).gain(0.5);
 
     // Scopes definition
     let mut monitor = Monitor::new();
@@ -92,25 +92,27 @@ async fn main() -> anyhow::Result<()> {
         .build()?;
 
     let metronome: Timer = Timer::new(100);
-    // Pyramid reconstructor
-    pymtor.h00_estimator()?;
-    let calibrator: Calibration<PyramidCalibrator> = pymtor.clone().into();
 
     actorscript!(
-        #[model(name=closed_loop )]
-        #[labels(metronome = "Metronome", calibrator = "Reconstructor", pym_ctrl="High-orders\nintegral controller")]
-        1: metronome[Tick] -> optical_model[GuideStar].. -> stats
-        1: stats[SegmentWfeRms<-9>].. -> segment_wfe_rms_scope
-        1: stats[WfeRms<-9>].. -> wfe_rms_scope
-        1: optical_model[GuideStar]
-            -> pyramid[DetectorFrame<f32>]
-                -> processor[PyramidMeasurements] -> calibrator
-        1: calibrator[ResidualM2modes]
-            -> pym_ctrl[M2modes]!
-                 -> optical_model
+    #[model(name=closed_loop )]
+    #[labels(metronome = "Metronome", calibrator = "Reconstructor", pym_ctrl="High-orders\nintegral controller")]
+        1: metronome[Tick]
+           -> optical_model[DetectorFrame]
+                -> processor[PyramidMeasurements]
+                    -> calibrator[ResidualM2modes]
+                        -> pym_ctrl[M2modes]!
+                             -> optical_model
+        // 1: optical_model[SegmentWfeRms<-9>].. -> segment_wfe_rms_scope
+        // 1: optical_model[WfeRms<-9>].. -> wfe_rms_scope
     );
 
-    monitor.await?;
+    let metronome: Timer = Timer::new(0);
+    actorscript!(
+        #[model(name=wavefront)]
+        1: metronome[Tick] -> optical_model[Wavefront]!$
+    );
+
+    // monitor.await?;
 
     Ok(())
 }
