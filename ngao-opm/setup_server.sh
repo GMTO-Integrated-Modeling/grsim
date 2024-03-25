29e0ce14dbdef68e631431b37b21eed28a2b9f53
Rod Conan <rconan@gmail.com>
2024-03-25 21:41:30 +0000

    NGAO-OPM TELESERVOS SERVER

--- ngao-opm/setup_server.sh
+++ ngao-opm/setup_server.sh
@@ -1,7 +1,7 @@
-# run in a terminal with: . setup.sh
+# run in a terminal with: . setup_server.sh
 export GMT_MODES_PATH=/home/ubuntu/mnt/ceo
 export FEM_REPO=/home/ubuntu/mnt/20230131_1605_zen_30_M1_202110_ASM_202208_Mount_202111/
 export MOUNT_MODEL=MOUNT_PDR_8kHz
 export FLOWCHART=neato
-export SERVOS_SERVER_IP=172.31.1.21
-export SERVOS_CLIENT_IP=44.235.124.92
+export TX_ADDRESS=172.31.1.21
+export RX_SERVER_ADDRESS=44.235.124.92
