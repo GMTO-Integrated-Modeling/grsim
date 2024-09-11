74e311c02352d27981839c3604f58718d42ac912
Rod Conan <rconan@gmto.org>
2024-09-11 00:51:59 +0000

    GRSIM/LTAO DFS WITH ATMOSPHERE

--- ltao/src/bin/dfs/main.rs
+++ ltao/src/bin/dfs/main.rs
@@ -1,4 +1,6 @@
-use crseo::{gmt::GmtM1, FromBuilder, Source};
+use std::fs::File;
+
+use crseo::{gmt::GmtM1, Atmosphere, FromBuilder, RayTracing, Source};
 use gmt_dos_actors::actorscript;
 use gmt_dos_clients::{print::Print, Gain, Sampler, Signal, Signals, Timer};
 use gmt_dos_clients_arrow::Arrow;
@@ -16,8 +18,8 @@ use gmt_dos_clients_io::{
 use interface::{Tick, Update, UID};
 use skyangle::Conversion;
 
-const DFS_CAMERA_EXPOSURE: usize = 1;
-const DFS_FFT_EXPOSURE: usize = 1;
+const DFS_CAMERA_EXPOSURE: usize = 10;
+const DFS_FFT_EXPOSURE: usize = 1000;
 
 type DFS = DispersedFringeSensor<DFS_CAMERA_EXPOSURE, DFS_FFT_EXPOSURE>;
 type DFS11 = DispersedFringeSensor<1, 1>;
@@ -26,26 +28,44 @@ type DFSP11 = DispersedFringeSensorProcessing<1, 1>;
 /*
 on-axis:  #    0: [[-1012, +2094, -3171, +4130, -5288, +6327]]
 8' off-axis:  #    0: [[-1004, +2047, -3183, +4160, -5269, +6254]]
+3 x 8' off-axis:  #    0: [[-1023, +2042, -3164, +4171, -5256, +6269]]
 */
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
+    let sampling_frequency = 1000f64;
+    let atm_builder = Atmosphere::builder().ray_tracing(
+        RayTracing::default()
+            .field_size(20f64.from_arcmin())
+            .duration(30.)
+            .filepath("dfs-atmosphere.bin"),
+    );
+
     let src_builder = Source::builder()
         .band("J")
         .size(3)
         .on_ring(8f32.from_arcmin());
 
-    let mut calib_m1_tz = <DFSP11 as Calibrate<GmtM1>>::calibrate(
-        OpticalModel::<DFS>::builder()
-            .source(src_builder.clone())
-            .sensor(DFS::builder().nyquist_factor(3.)),
-        CalibrationMode::RBM([None, None, Some(1e-6), None, None, None]),
-    )?;
-    calib_m1_tz.pseudoinverse();
-    println!("{calib_m1_tz}");
+    let calib_m1_tz = if let Ok(file) = File::open("calib_m1_tz.pkl") {
+        serde_pickle::from_reader(&file, Default::default()).unwrap()
+    } else {
+        let mut calib_m1_tz = <DFSP11 as Calibrate<GmtM1>>::calibrate(
+            OpticalModel::<DFS11>::builder()
+                .source(src_builder.clone())
+                .sensor(DFS11::builder().nyquist_factor(3.)),
+            CalibrationMode::RBM([None, None, Some(1e-6), None, None, None]),
+        )?;
+        calib_m1_tz.pseudoinverse();
+        println!("{calib_m1_tz}");
+        let mut file = File::create("calib_m1_tz.pkl")?;
+        serde_pickle::to_writer(&mut file, &calib_m1_tz, Default::default())?;
+        calib_m1_tz
+    };
 
     let om_builder = OpticalModel::<DFS>::builder()
+        .sampling_frequency(sampling_frequency)
         .source(src_builder.clone())
+        .atmosphere(atm_builder)
         .sensor(DFS::builder().nyquist_factor(3.));
     let mut om = om_builder.clone().build()?;
     let n = om.sensor().as_ref().unwrap().frame_size();
@@ -63,24 +83,32 @@ async fn main() -> anyhow::Result<()> {
         dfs_processor.set_reference(dfsp11.intercept());
     }
 
-    // let timer: Timer = Timer::new(1);
-    let print = Print::default();
+    {
+        let print = Print::default();
 
-    let sid = 5;
-    let m1_rbm = (1..=6).fold(Signals::new(42, 1), |s, sid| {
-        s.channel(
-            2 + (sid - 1) * 6,
-            (-1f64).powi(sid as i32) * (sid as f64) * 1e-6,
-        )
-    });
+        let sid = 5;
+        let m1_rbm = (1..=6).fold(Signals::new(42, 1000), |s, sid| {
+            s.channel(
+                2 + (sid - 1) * 6,
+                (-1f64).powi(sid as i32) * (sid as f64) * 1e-6,
+            )
+        });
 
-    let to_nm = Gain::new(vec![1e9; 6]);
+        let to_nm = Gain::new(vec![1e9; 6]);
 
-    actorscript!(
-        1: m1_rbm[M1RigidBodyMotions] -> om[DfsFftFrame<Dev>]
-            -> dfs_processor[Intercepts]
-                -> calib_m1_tz[SegmentPiston] -> to_nm[SegmentPiston] -> print
-    );
+        actorscript!(
+            1: m1_rbm[M1RigidBodyMotions] -> om
+            1000: om[DfsFftFrame<Dev>] -> dfs_processor[Intercepts]
+                    -> calib_m1_tz[SegmentPiston] -> to_nm[SegmentPiston] -> print
+        );
+    }
+
+    // let timer: Timer = Timer::new(1000);
+    // actorscript!(
+    //     #[model(name = dfs_frames)]
+    //     1: timer[Tick] -> om
+    //     100: om[DfsFftFrame<Host>]$
+    // );
 
     /*     // println!("DFS CAMERA FRAME");
     let mut log = logging_10.lock().await;
